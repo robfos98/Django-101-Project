57d064c8692e07d26e7e44da3bf3fca378e0a17b
Robert <robfos98@gmail.com>
2022-03-15 17:28:21 -0400

    Followed along

---
+++ feed/models.py
@@ -0,0 +1,22 @@
+from tkinter import CASCADE
+from django.db import models
+from sorl.thumbnail import ImageField
+
+class Comment(models.Model):
+    text = models.CharField(max_length=1023, blank=False, null=False)
+    post = models.ForeignKey('feed.Post', on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.text
+
+class Post(models.Model):
+    text = models.CharField(max_length=255, blank=False, null=False)
+    image = ImageField()
+
+    def __str__(self):
+        return self.text
+
+    # def comment_create(self):
+    #     self.comments.create(
+            
+    #     )
